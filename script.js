4f448c066ad48571d06e3d43ad322e2422dea878
satryawijayanto <satryawijayanto@gmail.com>
2026-07-20 18:13:57 +0700

    Trim education history to Bachelor's degree; remove unused/contradictory experience.json and script.js (index.html is source of truth for work history)

--- script.js
+++
@@ -1,8 +0,0 @@
-fetch('data/experience.json')
-  .then(res => res.json())
-  .then(data => {
-    const expDiv = document.getElementById('experience');
-    data.forEach(exp => {
-      expDiv.innerHTML += `<p><strong>${exp.company}</strong>: ${exp.position} (${exp.duration})</p>`;
-    });
-  });
